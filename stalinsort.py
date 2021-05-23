9d7b0e3b62867a5b793819570e70b5e1822bf9af
Allan <baldursgate3@gmail.com>
2021-05-23 12:50:32 +0100

    Update stalinsort.py

--- stalinsort.py
+++ stalinsort.py
@@ -1,6 +1,10 @@
-def stalinSort(x):
-    m = x[0]
-    for i in range(1, len(x)):
-        m = max(m, x[i])
-        if (m>x[i]):
-            x[i] = None
+def stalinSort(line):
+    # inspect the first comrade
+    comrade = line[0]
+    # go through the line
+    for i in range(1, len(line)):
+        comrade = max(comrade, line[i])
+        # decide which consecutive is out of order
+        if (comrade>line[i]):
+            # eliminate comrade which is out of order
+            line[i] = None
